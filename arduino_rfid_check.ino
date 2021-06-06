acfa868d4aac08987b341a224df3f7bb2785a07d
Jagmeet Singh <57046437+jagmeetz@users.noreply.github.com>
2021-06-06 22:10:50 +0530

    esp32 code finalised

--- arduino_rfid_check.ino
+++ arduino_rfid_check.ino
@@ -1,68 +1,92 @@
 
-#define RELAYPIN 13
-#define WARNLEDPIN 12
-char tag[] ="21007B61BC87"; // Replace with your own Tag ID
+#define WIFI_SSID "Chahal"
+#define WIFI_PASSWORD "84841906"
+
+
+#define totalStudents 5
+
+char tags[totalStudents][3][20] = {
+  //TagID, Name, RollNo
+  {"02005CF1812E", "Alfa", "1706700"},
+  {"18008DA1DFEB", "Beta", "1706701"},
+  {"21007BC1C853", "Gamma", "1706702"},
+  {"21007B868D51", "Theta", "1706703"},
+  {"02005DE8883F", "Nita", "1706704"},
+};
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+HTTPClient http;
 char input[12];        // A variable to store the Tag ID being presented
-int count = 0;        // A counter variable to navigate through the input[] character array
-boolean flag = 0;     // A variable to store the Tag match status
+int count = 0;         // A counter variable to navigate through the input[] character array
+int match = -1;
+
 void setup()
 {
-  Serial.begin(9600);   // Initialise Serial Communication with the Serial Monitor
-  pinMode(RELAYPIN,OUTPUT); // RELAY OUTPUT
-  pinMode(WARNLEDPIN,OUTPUT); //WRONG TAG INDICATOR
+  Serial.begin(9600);
+  pinMode(2, OUTPUT); //status LED
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 void loop()
 {
-  if(Serial.available())// Check if there is incoming data in the RFID Reader Serial Buffer.
-  {
-    count = 0; // Reset the counter to zero
-    /* Keep reading Byte by Byte from the Buffer till the RFID Reader Buffer is empty 
-       or till 12 Bytes (the ID size of our Tag) is read */
-    while(Serial.available() && count < 12) 
+  if ((WiFi.status() != WL_CONNECTED)) {
+    Serial.println("reconnecting...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    delay(5000);
+  }
+  else {
+    if (Serial.available()) // Check if there is incoming data in the RFID Reader Serial Buffer.
     {
-      input[count] = Serial.read(); // Read 1 Byte of data and store it in the input[] variable
-      count++; // increment counter
-      delay(5);
-    }
-    /* When the counter reaches 12 (the size of the ID) we stop and compare each value 
-        of the input[] to the corresponding stored value */
-    if(count == 12) // 
-    {
-      count =0; // reset counter varibale to 0
-      flag = 1;
-      /* Iterate through each value and compare till either the 12 values are 
-         all matching or till the first mistmatch occurs */
-      while(count<12 && flag !=0)  
+      count = 0;
+      while (Serial.available() && count < 12)
       {
-        if(input[count]==tag[count])
-        flag = 1; // everytime the values match, we set the flag variable to 1
+        input[count] = Serial.read(); // Read 1 Byte of data and store it in the input[] variable
+        count++;
+        delay(5);
+      }
+      if (count == 12)
+      {
+        for (int i = 0; i < totalStudents; i++) {
+          match = -1;
+          if (theseAreSame(input, tags[i][0]))
+          {
+            match = i;
+            break;
+          }
+        }
+        if (match == -1)
+        {
+          Serial.println("You are not the student of this class!");
+        }
         else
-        flag= 0; 
-                               /* if the ID values don't match, set flag variable to 0 and 
-                                  stop comparing by exiting the while loop */
-        count++; // increment i
+        {
+          Serial.println("Name:" + String(tags[match][1]) + " || RollNo:" + String(tags[match][2]) + " || TagID:" + String(tags[match][0]));
+
+          digitalWrite(2, HIGH);
+          http.begin("https://maker.ifttt.com/trigger/bme280_readings/with/key/bDJn3jlqZeR6xjh5Uu-aJNqLdc6ruEZj0EIm05PSLUr?value1=" + String(tags[match][2]) + "&value2=" + String(tags[match][1]));
+          int httpCode = http.GET();
+          if (httpCode > 0) {
+            String payload = http.getString();
+            Serial.println(payload);
+          }
+          else Serial.println("ERROR: failed to mark attendence");
+          http.end();
+          digitalWrite(2, LOW);
+
+        }
+        for (int i = 0; i < 12; i++)
+        {
+          input[i] = 'F';
+        }
       }
     }
-    if(flag == 1) // If flag variable is 1, then it means the tags match
-    {
-      Serial.println("Access Allowed!");
-      digitalWrite(RELAYPIN,HIGH);
-      delay (5000);
-      digitalWrite (RELAYPIN,LOW);
-    }
-    else
-    {
-      Serial.println("Access Denied"); // Incorrect Tag Message
-      digitalWrite(WARNLEDPIN,HIGH);
-      delay(5000);
-      digitalWrite(WARNLEDPIN,LOW);
-    }
-    /* Fill the input variable array with a fixed value 'F' to overwrite 
-    all values getting it empty for the next read cycle */
-    for(count=0; count<12; count++) 
-    {
-      input[count]= 'F';
-    }
-    count = 0; // Reset counter variable
   }
+}
+
+boolean theseAreSame(char input1[], char input2[]) { //Only compares 12 char arrays
+  for (int i = 0; i < 12; i++)
+  {
+    if (input1[i] != input2[i])return false;
+  }
+  return true;
 }
